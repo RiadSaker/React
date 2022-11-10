cfd34b5f6e976c3237430c64b6e03e0d93cbfa2b
RiadSaker <107034954+RiadSaker@users.noreply.github.com>
2022-11-10 02:47:26 +0100

    Add files via upload

---
+++ more-forms/src/App.js
@@ -0,0 +1,12 @@
+import MoreForms from './MoreForms';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MoreForms/>
+    </div>
+  );
+}
+
+export default App;
---
+++ more-forms/src/MoreForms.js
@@ -0,0 +1,99 @@
+import React, { useState} from 'react'
+
+const MoreForms = (props) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");  
+    const [confirmPass, setConfirmPassword] = useState("");
+    const [errorMsg, setErrorMsg] = useState("");
+    const [errorMsgOne, setErrorMsgOne] = useState("");
+    const [errorMsgTwo, setErrorMsgTwo] = useState("");
+    const [errorMsgThree, setErrorMsgThree] = useState("");
+    const [errorMsgFour, setErrorMsgFour] = useState("");
+
+
+
+
+    const formMessage = (val) => {
+        setFirstName(val);
+        if(val.length >= 2) {
+            setErrorMsg("");
+        }else{
+            setErrorMsg("First name must be at least 2 Characters")
+        }
+    }
+
+    const formMessageOne = (val) => {
+        setLastName(val);
+        if(val.length >= 2) {
+            setErrorMsgOne("");
+        }else{
+            setErrorMsgOne("Last name must be at least 2 Characters")
+        }
+    }
+    const formMessageTwo = (val) => {
+        setEmail(val);
+        if(val.length >= 2) {
+            setErrorMsgTwo("");
+        }else{
+            setErrorMsgTwo("Email must be at least 2 Characters")
+        }
+    }
+
+    const formMessageThree = (val) => {
+        setPassword(val);
+        if(val.length > 8){
+            setErrorMsgThree("");
+        }else{
+            setErrorMsgThree("Password must be at least 8 characters")
+        }
+    }
+
+    const formMessageFour = (val) => {
+
+        setConfirmPassword(val);
+        if(val !== password ){
+            setErrorMsgFour("Passwords must match")
+            
+        }else{
+            setErrorMsgFour("");
+
+        }
+    }
+    
+    return(
+        <div>
+            <form >
+                <div>
+                    <label>First Name:  </label> 
+                    <input type="text" onChange={ (e) => formMessage(e.target.value) } />
+                    <p>{errorMsg}</p>
+                </div>
+                <div>
+                    <label>Last Name: </label>
+                    <input type="text"onChange={ (e) => formMessageOne(e.target.value)}/>
+                    <p>{errorMsgOne}</p>
+                </div>
+                <div>
+                    <label>Email Address: </label> 
+                    <input type="text" onChange={ (e) => formMessageTwo(e.target.value) } />
+                    <p>{errorMsgTwo}</p>
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input type="text" onChange={ (e) => formMessageThree(e.target.value) } />
+                    <p>{errorMsgThree}</p>
+                </div>
+                <div>
+                    <p>{errorMsgFour}</p>
+                    <label>Confirm Password: </label>
+                    <input type="text" onChange={ (e) => formMessageFour(e.target.value)} />
+                </div>
+                
+            </form>
+        </div>
+    );
+};
+    
+export default MoreForms;
